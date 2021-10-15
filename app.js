c7616bc5d959f9aa67c8862ede99d289740209f6
Raul Plesa <raul.plesa@estudiantat.upc.edu>
2021-10-15 18:09:53 +0200

    Implemented MariaDB

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-// initialize
 const express = require('express');
 const app = express();
 
-// routers
-const auth = require('./user/router');
+// Routers
+const auth = require('./routes/auth.js');
 
-// routes
+// Routes
 app.use('/api/auth', auth);
 
-// middlewares
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+// Middlewares
+//app.use(notFoundMiddleware);
+//app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
+        //await connectDB(process.env.MONGO_URI);
         app.listen(port, () =>
             console.log(`Server is listening on port ${port}...`)
         );
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, UnauthenticatedError } = require('../errors');
-const User = require('../models/User');
 
 const register = async (req, res) => {
     res.send('user registered');
@@ -12,11 +11,11 @@ const register = async (req, res) => {
     // do we need to save it in our DB? yes, but only username and email (NO PW)
 
     // create new user with the info from req.body
-    const user = await User.create({ ...req.body });
+
     // user has been created successfully and we send back its
     // username and token
     res.status(StatusCodes.CREATED).json({
-        user: { email: user.getUsername().email },
+        user: { email: email },
         // token,
     });
 };
