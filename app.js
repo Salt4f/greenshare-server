780a0650e3e426fbc1702dbf823efa24237bf846
Raul Plesa <raul.plesa@estudiantat.upc.edu>
2021-10-16 09:44:04 +0200

    Added error handling middlewares

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 
-// Middlewares
 app.use(express.json());
-//app.use(notFoundMiddleware);
-//app.use(errorHandlerMiddleware);
 
 // Routers
 const auth = require('./routes/auth.js');
 
+// Error handlers
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 // Routes
 app.use('/api/auth', auth);
+// example of auth middleware:  app.use('/api/posts/', authenticateUser, jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
