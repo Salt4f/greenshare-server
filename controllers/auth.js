79a021a7ce8f9f15ad79f0ac94ff9ee216a0c4f1
obnol <longboqiu@hotmail.com>
2021-10-16 18:34:54 +0200

    Re-structured error handling

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,48 +1,45 @@
-const {
-    StatusCodes
-} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const {
     BadRequestError,
-    UnauthenticatedError
+    UnauthenticatedError,
+    InternalServerError,
 } = require('../errors');
 const {
     registerRequest,
-    loginRequest
+    loginRequest,
 } = require('../requests/stubs/user-service'); // remove '/stubs' for prod
-const {
-    createUser
-} = require('../db/users');
-const {
-    request
-} = require('express');
+const { createUser } = require('../db/users');
+const { request } = require('express');
 
 const register = (req, res) => {
-    const {
-        email,
-        password,
-        nickname
-    } = req.body;
+    const { email, password, nickname } = req.body;
+
+    if (!email || !password || !nickname) {
+        throw new BadRequestError('Wrong parameters');
+    }
 
     registerRequest(email, password, nickname)
         .then((response) => {
             if (response.status == StatusCodes.OK) {
                 createUser(response.data.id, email, nickname)
                     .then(() => {
-                        console.log('Successfully created user, sending response...');
+                        console.log(
+                            'Successfully created user, sending response...'
+                        );
                         res.status(StatusCodes.CREATED).json({
                             id: response.data.id,
-                            token: response.data.token
+                            token: response.data.token,
                         });
                     })
-                    .catch(error => {
-                        console.log("Couldn't create user, sending bad response");
+                    .catch((error) => {
+                        console.log(
+                            "Couldn't create user, sending bad response"
+                        );
                         console.log(`ERROR: ${error}`);
-                        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                            error: `Couldn't save user in database`
-                        });
+                        throw new InternalServerError(`Couldn't create user`);
                     });
             } else {
-                res.status(StatusCodes.BAD_REQUEST).send();
+                throw new InternalServerError(`User service error`);
             }
         })
         .catch((error) => {
@@ -51,21 +48,21 @@ const register = (req, res) => {
 };
 
 const login = (req, res) => {
+    const { email, password } = req.body;
 
-    const {
-        email,
-        password
-    } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError('Wrong parameters');
+    }
 
     loginRequest(email, password)
         .then(function (response) {
             if (response.status == StatusCodes.OK) {
                 res.status(StatusCodes.OK).json({
                     id: response.data.id,
-                    token: response.data.token
+                    token: response.data.token,
                 });
             } else {
-                res.status(StatusCodes.UNAUTHORIZED);
+                throw new UnauthenticatedError(`Unauthorized`);
             }
         })
         .catch(function (error) {
@@ -75,5 +72,5 @@ const login = (req, res) => {
 
 module.exports = {
     register,
-    login
+    login,
 };
