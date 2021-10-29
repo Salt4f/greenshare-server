8ca3de1d7a21c5d5515fe315a3d45389217d3283
obnol <longboqiu@hotmail.com>
2021-10-29 11:30:36 +0200

    TG-180 #In revision

--- requests/user-service.js
+++ requests/user-service.js
@@ -7,7 +7,7 @@ const registerRequest = async (email, password, nickname) => {
     try {
         const response = await axios({
             method: 'post',
-            url: 'http://users.vgafib.org/users/',
+            url: process.env.USERSERVICE_REGISTER_URL,
             responseType: 'json',
             data: {
                 email: email,
@@ -25,7 +25,7 @@ const loginRequest = async (email, password) => {
     try {
         const response = await axios({
             method: 'post',
-            url: 'http://users.vgafib.org/login/',
+            url: process.env.USERSERVICE_LOGIN_URL,
             responseType: 'json',
             data: {
                 email: email,
@@ -42,7 +42,7 @@ const tokenValidationRequest = async (id, token) => {
     try {
         const response = await axios({
             method: 'post',
-            url: 'http://users.vgafib.org/verify-user/',
+            url: process.env.USERSERVICE_TOKEN_VALIDATION_URL,
             responseType: 'json',
             data: {
                 id: id,
