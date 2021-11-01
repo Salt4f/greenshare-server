b13b1a255b93a7d30d173cea469828c16161d3bf
obnol <longboqiu@hotmail.com>
2021-11-01 20:07:55 +0100

    Added 1st version of auth

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,5 +1,24 @@
-const auth = async (req, res, next) => {
-    next();
+const { tokenValidationRequest } = require('../requests/user-service');
+const logger = require('../utils/logger');
+
+const authenticateUser = async (req, res, next) => {
+    try {
+        const response = await tokenValidationRequest(
+            req.body.ownerId,
+            req.body.token
+        );
+        if (response.status == StatusCodes.CREATED) {
+            logger.log(`User successfuly validated, sending response...`, 1);
+
+            next();
+        } else {
+            logger.log(`Invalid token or ownerId, sending response...`, 1);
+            // res.status(StatusCodes.UNAUTHORIZED).send();
+            throw new Error('UNAUTHORIZED');
+        }
+    } catch (error) {
+        logger.log(error.message, 0);
+    }
 };
 
-module.exports = auth;
+module.exports = authenticateUser;
