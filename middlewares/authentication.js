88a7203dca8d426bd21ae5f3d3b1fb2202d15ece
obnol <longboqiu@hotmail.com>
2021-11-03 20:38:23 +0100

    Updated auth

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,19 +1,21 @@
 const { tokenValidationRequest } = require('../requests/user-service');
 const logger = require('../utils/logger');
+const db = require('../db/connect');
 
 const authenticateUser = async (req, res, next) => {
     try {
         const response = await tokenValidationRequest(
-            req.body.ownerId,
+            req.body.id,
             req.body.token
         );
         if (response.status == StatusCodes.CREATED) {
+            // check id in our db
+            await db.users.findOne({ where: { id: req.body.id } });
             logger.log(`User successfuly validated, sending response...`, 1);
 
             next();
         } else {
             logger.log(`Invalid token or ownerId, sending response...`, 1);
-            // res.status(StatusCodes.UNAUTHORIZED).send();
             throw new Error('UNAUTHORIZED');
         }
     } catch (error) {
