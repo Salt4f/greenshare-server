59d13a6ab743bb4b133eb6d8f47e68a59039533c
obnol <longboqiu@hotmail.com>
2021-12-31 16:14:28 +0100

    Fixed minors

--- utils/logger.js
+++ utils/logger.js
@@ -3,7 +3,11 @@ const logger = {
         // level: 0=error 1=info 2=finest
         date = new Date().toLocaleString('es-MA');
         const tag = level == 0 ? 'ERROR' : level == 1 ? 'INFO' : 'FINEST';
-        console.log('\x1b[32m%s\x1b[0m', `[${date}] [${tag}]: ${message}`);
+        if (level === 0) {
+            console.log('\x1b[31m%s\x1b[0m', `[${date}] [${tag}]: ${message}`);
+        } else {
+            console.log('\x1b[32m%s\x1b[0m', `[${date}] [${tag}]: ${message}`);
+        }
     },
 };
 
